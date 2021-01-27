8366d52a721b50d832a8b1b81bfe15bab47beb7f
Pozadr <adpozor@gmail.com>
2021-01-27 19:51:05 +0100

    init

---
+++ src/main/java/pl/pozadr/map/controller/MapController.java
@@ -0,0 +1,13 @@
+package pl.pozadr.map.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MapController {
+
+    @GetMapping("/map")
+    public String getMap() {
+        return "map";
+    }
+}
